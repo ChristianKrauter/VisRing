ed41516aee9484fc4d0eb5d36776d784274af7ae
Christian Krauter <chris.a.krauter@gmail.com>
2024-06-30 23:51:57 +0200

    Adds documentation

--- src/visring.cpp
+++ src/visring.cpp
@@ -1,5 +1,10 @@
 #include <VisRing.h>
 
+/// @brief Scales values in array to target range.
+/// @param target_min Lower bound of target range.
+/// @param target_max Upper bound of target range.
+/// @param values Array to be scaled.
+/// @param values_count Number of items in array.
 void VisRing::scaleToMinMax(int target_min, int target_max, int values[], int values_count)
 {
   int values_dif = 0;
@@ -28,6 +33,13 @@ void VisRing::scaleToMinMax(int target_min, int target_max, int values[], int va
   }
 }
 
+/// @brief Scales values in array with known value range to target range.
+/// @param target_min Lower bound of target range.
+/// @param target_max Upper bound of target range.
+/// @param value_range_min Lower bound of values.
+/// @param value_range_max Upper bound of values.
+/// @param values Array to be scaled.
+/// @param values_count Number of items in array.
 void VisRing::scaleToMinMaxKnownRange(int target_min, int target_max, int value_range_min, int value_range_max, int values[], int values_count)
 {
   int values_dif = value_range_max - value_range_min;
@@ -40,6 +52,10 @@ void VisRing::scaleToMinMaxKnownRange(int target_min, int target_max, int value_
   }
 }
 
+/// @brief Draws a heart icon.
+/// @param x Value of lowest x value (left edge).
+/// @param y Value of highest y value (upper edge).
+/// @param grayscale Grayscale value between 0 and 15.
 void VisRing::drawHeart(int x, int y, int grayscale)
 {
   SSD1320::lineGS(x, y - 2, x, y - 6, grayscale);
@@ -59,6 +75,10 @@ void VisRing::drawHeart(int x, int y, int grayscale)
   SSD1320::lineGS(x + 12, y - 2, x + 12, y - 6, grayscale);
 }
 
+/// @brief Draws an envelope icon.
+/// @param x Value of lowest x value (left edge).
+/// @param y Value of lowest y value (lower edge).
+/// @param grayscale Grayscale value between 0 and 15.
 void VisRing::drawEnvelope(int x, int y, int grayscale)
 {
   SSD1320::lineGS(x, y, x, y + 10, grayscale);
@@ -69,25 +89,27 @@ void VisRing::drawEnvelope(int x, int y, int grayscale)
   SSD1320::lineGS(x + 20, y + 10, x + 10, y + 5, grayscale);
 }
 
+/// @brief Draws full-screen values.
+/// @param type Variabble selection pattern. 0 = Hatching, 1 = Band, 2 = Circles.
 void VisRing::drawPattern(int type)
 {
   switch (type)
   {
-  case 0:
+  case 0: /// Hatching
     for (int i = 0; i <= SSD1320::getDisplayWidth() / 10; i++)
     {
       SSD1320::lineGS(i * 10, 0, (i + 1) * 10, 30, i % 14 + 1);
       SSD1320::lineGS((i + 1) * 10, 0, i * 10, 30, (16 - i) % 14 + 1);
     }
     break;
-  case 1:
+  case 1: /// Band
     for (int i = 0; i < SSD1320::getDisplayHeight() / 2; i++)
     {
       SSD1320::lineGS(0, i, 160, i, i);
       SSD1320::lineGS(0, i + 15, 160, i + 15, (16 - i));
     }
     break;
-  case 2:
+  case 2: /// Circles
     for (int i = 0; i < 25; i++)
     {
       SSD1320::circleGS(random(0, 160), random(0, 32), random(2, 15), random(0, 15));
@@ -96,6 +118,10 @@ void VisRing::drawPattern(int type)
   }
 }
 
+/// @brief Draws scaled bar chart for horizontal screen orientation.
+/// @param values Heights of the bars.
+/// @param values_count Number of bars.
+/// @param grayscale Grayscale value between 0 and 15.
 void VisRing::drawBarChartHor(int values[], int values_count, int grayscale)
 {
   if (values_count > 80 | values_count < 2)
@@ -113,6 +139,10 @@ void VisRing::drawBarChartHor(int values[], int values_count, int grayscale)
   }
 }
 
+/// @brief Draws scaled bar chart for vertical screen orientation.
+/// @param values Heights of the bars.
+/// @param values_count Number of bars.
+/// @param grayscale Grayscale value between 0 and 15.
 void VisRing::drawBarChartVert(int values[], int values_count, int grayscale)
 {
   if (values_count > 16 | values_count < 2)
@@ -130,6 +160,15 @@ void VisRing::drawBarChartVert(int values[], int values_count, int grayscale)
   }
 }
 
+/// @brief Draws scaled small multiples bar charts.
+/// @param values Height of the bars. 2D. Max charts = 7, max values = 16.
+/// @param charts_count Number of charts.
+/// @param values_counts Number of values of each chart.
+/// @param focus_charts Indicator of focused charts (different grayscale).
+/// @param value_range_min Lower bound of values.
+/// @param value_range_max Upper bound of values.
+/// @param grayscale Grayscale value between 0 and 15.
+/// @param grayscale_focus Grayscale value of focused chart between 0 and 15.
 void VisRing::drawBarChartVertSmallMultiples(int values[7][16], int charts_count, int values_counts[7], bool focus_charts[7], int value_range_min, int value_range_max, int grayscale, int grayscale_focus)
 {
 
@@ -164,6 +203,10 @@ void VisRing::drawBarChartVertSmallMultiples(int values[7][16], int charts_count
   }
 }
 
+/// @brief Draws scaled line chart for horizontal screen orientation.
+/// @param values Heights of the line.
+/// @param values_count Number of values.
+/// @param grayscale Grayscale value between 0 and 15.
 void VisRing::lineChart(int values[], int values_count, int grayscale)
 {
   if (values_count > 160 | values_count < 2)
@@ -180,6 +223,13 @@ void VisRing::lineChart(int values[], int values_count, int grayscale)
   }
 }
 
+/// @brief Draws pixels for partial circle in 8 discreet steps. After Bresenham.
+/// @param x0 Pixel x position.
+/// @param y0 Pixel y position.
+/// @param x Pixel x offset.
+/// @param y Pixel y offset.
+/// @param parts Number of segments to draw.
+/// @param grayscale Grayscale value between 0 and 15.
 void VisRing::drawCircleSegment(int x0, int y0, int x, int y, int parts, int grayscale)
 {
   if (parts >= 1)
@@ -223,6 +273,15 @@ void VisRing::drawCircleSegment(int x0, int y0, int x, int y, int parts, int gra
   }
 }
 
+/// @brief Draws discreet radial progres chart.
+/// @param x0 Center position of circle.
+/// @param y0 Center position of circle.
+/// @param radius Radius of circle.
+/// @param parts Number of parts to draw (12.5% steps).
+/// @param show_percentage Show percentage inside the chart.
+/// @param grayscale_background Grayscale value of remaining circle between 0 and 15.
+/// @param grayscale Grayscale value between 0 and 15.
+/// @param grayscale_percentage Grayscale value of percentage between 0 and 15.
 void VisRing::radialProgressChart(int x0, int y0, int radius, int parts, bool show_percentage, int grayscale_background, int grayscale, int grayscale_percentage)
 {
   int d = 3 - 2 * radius;
@@ -272,6 +331,9 @@ void VisRing::radialProgressChart(int x0, int y0, int radius, int parts, bool sh
   }
 }
 
+/// @brief Draws notification cound with text.
+/// @param number Number of notifications.
+/// @param grayscale Grayscale value between 0 and 15.
 void VisRing::showNotification(int number, int grayscale)
 {
   String text = String(number);
@@ -288,12 +350,19 @@ void VisRing::showNotification(int number, int grayscale)
   SSD1320::printStringGS(25, 10, text, grayscale, 0);
 }
 
+/// @brief Draws notification with envelope icon.
+/// @param number Number of notifications.
+/// @param grayscale Grayscale value between 0 and 15.
 void VisRing::showNotificationIcon(int number, int grayscale)
 {
   SSD1320::printStringGS(85, 5, String(number), grayscale, 1);
   drawEnvelope(60, 9, grayscale);
 }
 
+/// @brief Draws heart rate with heart icon.
+/// @param hr Heart rate value.
+/// @param grayscale Grayscale value between 0 and 15.
+/// @param grayscale_heart Grayscale value of heart between 0 and 15.
 void VisRing::drawHR(int hr, int grayscale, int grayscale_heart)
 {
   String text = String(hr) + " bpm";
