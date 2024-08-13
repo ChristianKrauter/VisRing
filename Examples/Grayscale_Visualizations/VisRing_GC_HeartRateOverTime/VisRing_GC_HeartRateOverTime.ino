f0f0e6c9b529e537b1ea7f11d3c7c26c9960bc76
Christian Krauter <chris.a.krauter@gmail.com>
2024-08-13 15:55:16 +0200

    Updates HROT vis with new data

--- Examples/Grayscale_Visualizations/VisRing_GC_HeartRateOverTime/VisRing_GC_HeartRateOverTime.ino
+++ Examples/Grayscale_Visualizations/VisRing_GC_HeartRateOverTime/VisRing_GC_HeartRateOverTime.ino
@@ -20,18 +20,36 @@ void setup() {
 }
 
 void example_HearRateOverTime() {
-  int values_count = 80;
-  // Copilot numbers 60 - 100
-  // int hr[values_count] = { 72, 85, 91, 78, 94, 67, 82, 76, 88, 73, 97, 69, 81, 79, 92, 70, 83, 75, 89, 68, 84, 77, 93, 71, 86, 80, 95, 66, 87, 74, 90, 65, 96, 63, 98, 64, 99, 62, 100, 61, 97, 63, 95, 66, 92, 68, 89, 70, 87, 71, 85, 72, 83, 73, 81, 74, 79, 75, 77, 76, 78, 80, 76, 82, 75, 84, 74, 86, 73, 88, 72, 90, 71, 91, 70, 92, 69, 93, 68, 94 };
-  // Copilot numbers 0 - 180
-  // int hr[values_count] = { 112, 129, 147, 92, 163, 75, 138, 120, 156, 81, 174, 69, 141, 102, 167, 60, 132, 84, 150, 66, 144, 96, 159, 72, 135, 78, 153, 63, 126, 90, 165, 57, 177, 54, 180, 51, 179, 48, 178, 45, 176, 42, 174, 39, 172, 36, 170, 33, 168, 30, 166, 27, 164, 24, 162, 21, 160, 18, 158, 15, 156, 12, 154, 9, 152, 6, 150, 3, 148, 0, 146, 3, 144, 6, 142, 9, 140, 12, 138, 15 };
-  // RNG
-  int hr[values_count] = { 107, 151, 51, 162, 85, 164, 57, 130, 38, 110, 163, 181, 177, 178, 71, 84, 75, 23, 97, 158, 163, 78, 121, 160, 11, 100, 74, 116, 120, 181, 53, 63, 98, 78, 8, 114, 115, 5, 135, 14, 180, 90, 84, 189, 186, 141, 13, 133, 113, 71, 182, 153, 177, 62, 78, 98, 125, 65, 42, 96, 80, 31, 174, 92, 138, 88, 70, 159, 185, 133, 26, 40, 84, 92, 141, 175, 63, 162, 137, 58 };
-  VisRing.lineChart(hr, values_count, true, 0, 200, 15);
-  VisRing.printStringGS(80, 15, String(hr[40]), true, 0, 0);
+  int values_count = 24;
+
+  int hr_standing[values_count] = { 83, 83,  59,  76,  88, 84,  70, 84, 71, 69,  90, 113, 120, 89,  89, 95,  84, 94,  83, 102, 72,  72,  85,  102 };
+  int hr_sitting[values_count] =  { 53, 83,  85,  80,  92, 87,  59, 96, 91, 87,  93, 89,   85, 84,  96, 85,  91, 88,  92, 85 , 83,  89,  82,  92  };
+  //int hr_walking[values_count] =  { 83, 103, 105, 120, 92, 170, 99, 96, 91, 112, 93, 100, 104, 101, 96, 120, 91, 108, 92, 105, 103, 109, 102, 92 };
+  int hr_walking[values_count] =    { 30,30,30,30,30,30,30,30,30,30,30, 30, 200, 101, 96, 120, 91, 108, 92, 105, 103, 109, 102, 92 };
+
+  /*
+  VisRing.printStringGS(84, 20, String(hr_standing[values_count / 2]), true, 15, 0);
+  VisRing.lineChart(hr_standing, values_count, true, 0, 200, 15);
+  VisRing.lineVGS(80, 0, 32, 7);
   VisRing.displayGS();
-  delay(500);
+  delay(2000);
   VisRing.clearDisplayGS(2);
+
+  VisRing.printStringGS(84, 20, String(hr_sitting[values_count / 2]), true, 15, 0);
+  VisRing.lineChart(hr_sitting, values_count, true, 0, 200, 15);
+  VisRing.lineVGS(80, 0, 32, 7);
+  VisRing.displayGS();
+  delay(2000);
+  VisRing.clearDisplayGS(2);
+  */
+
+  VisRing.printStringGS(75, 20, String(hr_walking[values_count / 2]), true, 15, 0);
+  VisRing.lineChart(hr_walking, values_count, true, 0, 200, 15);
+  VisRing.lineVGS(72, 0, 32, 7);
+  VisRing.displayGS();
+  delay(2000);
+  VisRing.clearDisplayGS(2);
+
 }
 
 void loop() {
