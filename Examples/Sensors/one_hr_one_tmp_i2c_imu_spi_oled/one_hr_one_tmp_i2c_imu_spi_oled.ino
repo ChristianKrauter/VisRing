8849bd5cf38e9bc4335acceda7ae33cf9bf5385f
Christian Krauter <christian.krauter@visus.uni-stuttgart.de>
2025-02-28 17:50:46 +0100

    formatting

--- Examples/Sensors/one_hr_one_tmp_i2c_imu_spi_oled/one_hr_one_tmp_i2c_imu_spi_oled.ino
+++ Examples/Sensors/one_hr_one_tmp_i2c_imu_spi_oled/one_hr_one_tmp_i2c_imu_spi_oled.ino
@@ -31,10 +31,10 @@
 #include "ArduinoJson.h"
 #include <SPI.h>
 
-#include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
-#define CS_PIN 2     // Which pin you connect CS to. Used only when "USE_SPI" is defined
-ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
-Adafruit_TMP117  tmp117;
+#include "ICM_20948.h"  // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
+#define CS_PIN 2        // Which pin you connect CS to. Used only when "USE_SPI" is defined
+ICM_20948_SPI myICM;    // If using SPI create an ICM_20948_SPI object
+Adafruit_TMP117 tmp117;
 
 long display_time = millis();
 
@@ -50,7 +50,7 @@ uint32_t rxLastTime = 0;
 const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
-long lastBeat = 0; //Time at which the last beat occurred
+long lastBeat = 0;  //Time at which the last beat occurred
 
 float beatsPerMinute;
 int beatAvg;
@@ -71,35 +71,33 @@ float ALL_MAX[14] = {
 
 int COUNT;
 #include <SSD1320_OLED.h>
-SSD1320 flexibleOLED(15, 16, 12, 13); // NRF52832 15 = CS, 16 = RES, 13 = SCLK, 11 = SDIN
+SSD1320 flexibleOLED(15, 16, 12, 13);  // NRF52832 15 = CS, 16 = RES, 13 = SCLK, 11 = SDIN
 
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Serial.println("MAX30105 Basic Readings Example");
 
   Wire.setClock(3400000);
   Wire.begin();
   SPI.begin();
-  flexibleOLED.begin(160, 32); //Display is 160 wide, 32 high
+  flexibleOLED.begin(160, 32);  //Display is 160 wide, 32 high
   //Setup to sense up to 18 inches, max LED brightness
-  byte ledBrightness = 0xFF; //Options: 0=Off to 255=50mA
-  byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
-  byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  int sampleRate = 1600; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 411; //Options: 69, 118, 215, 411
-  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+  byte ledBrightness = 0xFF;  //Options: 0=Off to 255=50mA
+  byte sampleAverage = 1;     //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 3;           //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int sampleRate = 1600;      //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411;       //Options: 69, 118, 215, 411
+  int adcRange = 16384;       //Options: 2048, 4096, 8192, 16384
 
   // Initialize sensor
-  if (particleSensor.begin(Wire, I2C_SPEED_FAST, 0x57) == false)
-  {
+  if (particleSensor.begin(Wire, I2C_SPEED_FAST, 0x57) == false) {
     Serial.println("MAX30105 was not found. Please check wiring/power. ");
-    while (1);
+    while (1)
+      ;
   }
 
-
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor. Use 6.4mA for LED drive
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);  //Configure sensor. Use 6.4mA for LED drive
   particleSensor.setPulseAmplitudeRed(0x50);
   particleSensor.setPulseAmplitudeIR(0x50);
   particleSensor.setPulseAmplitudeGreen(0x96);
@@ -120,24 +118,20 @@ void setup()
   SPI.begin();
   //  SPI.beginTransaction(SPISettings(7000000, MSBFIRST, SPI_MODE0));
 
-  myICM.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+  myICM.enableDebugging();  // Uncomment this line to enable helpful debug messages on Serial
 
   bool initialized = false;
-  while (!initialized)
-  {
+  while (!initialized) {
 
     myICM.begin(CS_PIN, SPI);
 
-    myICM.startupDefault(false); // Force a full - not minimal - startup
+    myICM.startupDefault(false);  // Force a full - not minimal - startup
     Serial.print(F("Initialization of the sensor returned: "));
     Serial.println(myICM.statusString());
-    if (myICM.status != ICM_20948_Stat_Ok)
-    {
+    if (myICM.status != ICM_20948_Stat_Ok) {
       Serial.println("Trying again...");
       delay(500);
-    }
-    else
-    {
+    } else {
       initialized = true;
     }
   }
@@ -158,10 +152,10 @@ void setup()
   Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
 
   Bluefruit.begin();
-  Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
+  Bluefruit.setTxPower(4);  // Check bluefruit.h for supported values
   Bluefruit.Periph.setConnectCallback(connect_callback);
   Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
-  Bluefruit.Periph.setConnInterval(6, 12); // 7.5 - 15 ms
+  Bluefruit.Periph.setConnInterval(6, 12);  // 7.5 - 15 ms
   Bluefruit.setName("BAC_Ring#1");
   // Configure and Start Device Information Service
   bledis.setManufacturer("Adafruit Industries");
@@ -180,27 +174,26 @@ void setup()
   Serial.println("Once connected, enter character(s) that you wish to send");
 
 
-  flexibleOLED.clearDisplay(); //Clear display and buffer
+  flexibleOLED.clearDisplay();  //Clear display and buffer
 
-  flexibleOLED.setFontType(0); //Large font
+  flexibleOLED.setFontType(0);  //Large font
   flexibleOLED.setCursor(15, 12);
   flexibleOLED.print("Hi VisRing");
 
-  flexibleOLED.setFontType(0); //Small font
+  flexibleOLED.setFontType(0);  //Small font
   flexibleOLED.setCursor(80, 0);
   flexibleOLED.print("PSU x VISVAR");
 
   flexibleOLED.display();
 }
 
-void display_tmp(void)
-{
+void display_tmp(void) {
   long diff_display = millis() - display_time;
   //  Serial.println(diff_display);
   //  delay(5000);
   if (diff_display > 3000) {
-    flexibleOLED.clearDisplay(); //Clear display and buffer
-    flexibleOLED.setFontType(0); //Large font
+    flexibleOLED.clearDisplay();  //Clear display and buffer
+    flexibleOLED.setFontType(0);  //Large font
     flexibleOLED.setCursor(20, 12);
     flexibleOLED.print("Tmp:  ");
     flexibleOLED.print(ALL_MAX[total_num_data - 2]);
@@ -211,7 +204,7 @@ void display_tmp(void)
 }
 
 void startAdv(void)
-{
+void startAdv(void) {
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
 
@@ -232,13 +225,12 @@ void startAdv(void)
      https://developer.apple.com/library/content/qa/qa1931/_index.html
   */
   Bluefruit.Advertising.restartOnDisconnect(true);
-  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
-  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
-  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
+  Bluefruit.Advertising.setInterval(32, 244);  // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);    // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);              // 0 = Don't stop advertising after n seconds
 }
 
-void connect_callback(uint16_t conn_handle)
-{
+void connect_callback(uint16_t conn_handle) {
   BLEConnection* conn = Bluefruit.Connection(conn_handle);
   Serial.println("Connected");
 
@@ -266,26 +258,23 @@ void connect_callback(uint16_t conn_handle)
    @param conn_handle connection where this event happens
    @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
 */
-void disconnect_callback(uint16_t conn_handle, uint8_t reason)
-{
-  (void) conn_handle;
-  (void) reason;
+void disconnect_callback(uint16_t conn_handle, uint8_t reason) {
+  (void)conn_handle;
+  (void)reason;
   Serial.print("total count:");
   Serial.println(COUNT);
   Serial.println();
-  Serial.print("Disconnected, reason = 0x"); Serial.println(reason, HEX);
-
+  Serial.print("Disconnected, reason = 0x");
+  Serial.println(reason, HEX);
 }
 
-void bleuart_rx_callback(uint16_t conn_hdl)
-{
-  (void) conn_hdl;
+void bleuart_rx_callback(uint16_t conn_hdl) {
+  (void)conn_hdl;
 
   rxLastTime = millis();
 
   // first packet
-  if ( rxCount == 0 )
-  {
+  if (rxCount == 0) {
     rxStartTime = millis();
   }
 
@@ -297,10 +286,8 @@ void bleuart_rx_callback(uint16_t conn_hdl)
   // Serial.printf("RX %d bytes\n", count);
 }
 
-void bleuart_notify_callback(uint16_t conn_hdl, bool enabled)
-{
-  if ( enabled )
-  {
+void bleuart_notify_callback(uint16_t conn_hdl, bool enabled) {
+  if (enabled) {
     Serial.println("Send a key and press enter to start test");
   }
 }
@@ -313,7 +300,7 @@ void clear_all_max() {
 
 void get_ppg_data(void) {
   particleSensor.check();
-  while (particleSensor.available()) //do we have new data?
+  while (particleSensor.available())  //do we have new data?
   {
     ALL_MAX[0] = particleSensor.getFIFORed();
     ALL_MAX[1] = particleSensor.getFIFOIR();
@@ -327,22 +314,20 @@ void get_hr_data(void) {
   particleSensor.check();
   long irValue = particleSensor.getIR();
 
-  if (checkForBeat(irValue) == true)
-  {
+  if (checkForBeat(irValue) == true) {
     //We sensed a beat!
     long delta = millis() - lastBeat;
     lastBeat = millis();
 
     beatsPerMinute = 60 / (delta / 1000.0);
 
-    if (beatsPerMinute < 255 && beatsPerMinute > 20)
-    {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
+    if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+      rates[rateSpot++] = (byte)beatsPerMinute;  //Store this reading in the array
+      rateSpot %= RATE_SIZE;                     //Wrap variable
 
       //Take average of readings
       beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
+      for (byte x = 0; x < RATE_SIZE; x++)
         beatAvg += rates[x];
       beatAvg /= RATE_SIZE;
     }
@@ -350,81 +335,71 @@ void get_hr_data(void) {
   ALL_MAX[0] = irValue;
   ALL_MAX[1] = beatsPerMinute;
   ALL_MAX[2] = beatAvg;
-
 }
 
 void get_imu_data(void) {
 
-  if (myICM.dataReady())
-  {
-    myICM.getAGMT();         // The values are only updated when you call 'getAGMT'
-    float ALL_IMU[] = {myICM.accX() * 0.01, myICM.accY() * 0.01, myICM.accZ() * 0.01, // acceleration is measured in m/s^2
-                       myICM.gyrX(),  myICM.gyrY(), myICM.gyrZ(), // magnetometer is measured in uT
-                       myICM.magX(), myICM.magY(), myICM.magZ(),// gyro is measured in radians/s
-                      };
+  if (myICM.dataReady()) {
+    myICM.getAGMT();  // The values are only updated when you call 'getAGMT'
+    float ALL_IMU[] = {
+      myICM.accX() * 0.01, myICM.accY() * 0.01, myICM.accZ() * 0.01,  // acceleration is measured in m/s^2
+      myICM.gyrX(), myICM.gyrY(), myICM.gyrZ(),                       // magnetometer is measured in uT
+      myICM.magX(), myICM.magY(), myICM.magZ(),                       // gyro is measured in radians/s
+    };
 
 
     for (int i = 0; i < 9; i++) {
       ALL_MAX[3 + i] = ALL_IMU[i];
     }
 
-  }
-  else
-  {
+  } else {
     Serial.println("Waiting for data");
     delay(500);
   }
 }
 
 void get_tmp_data() {
-  sensors_event_t temp; // create an empty event to be filled
+  sensors_event_t temp;  // create an empty event to be filled
   tmp117.getEvent(&temp);
   ALL_MAX[total_num_data - 2] = temp.temperature;
 }
 
-void loop()
-{
-  if (Bluefruit.connected() && bleuart.notifyEnabled())
-  {
+void loop() {
+  if (Bluefruit.connected() && bleuart.notifyEnabled()) {
     send_time = millis();
     clear_all_max();
     get_imu_data();
     get_hr_data();
     get_tmp_data();
-    if (ALL_MAX[0] != -1)
-    {
+    if (ALL_MAX[0] != -1) {
       ALL_MAX[total_num_data - 1] = send_time;
       //      print_all_max();
       display_tmp();
       send_message();
       COUNT += 1;
     }
-
   }
 
   if (!Bluefruit.connected()) {
     COUNT = 0;
   }
 
-  delay(1 );
+  delay(1);
 }
-
-
-
 
 void print_all_max() {
   Serial.print("[");
   for (int i = 0; i < total_num_data; i++) {
-    Serial.print(ALL_MAX[i]); Serial.print(",");
+    Serial.print(ALL_MAX[i]);
+    Serial.print(",");
   }
   Serial.println("]");
 }
 
-void send_message(void)
-{
+void send_message(void) {
   //  int packet_size = total_num_data - 1;
   int total_size = total_num_data * 4;
-  byte byteArray[total_size];// 10+1
+  byte byteArray[total_size];  // 10+1
 
 
   for (int i = 0; i < total_num_data; i++) {
@@ -435,5 +410,4 @@ void send_message(void)
   }
 
   size_t sent_data_size = bleuart.write(byteArray, sizeof(byteArray));
-
 }
